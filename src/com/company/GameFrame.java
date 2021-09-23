97a4dfcbd7afbb5a1d3d37a55d8b22592a6c8302
Vladimir Vorozhtcov <Lilith.2014@mail.ru>
2021-09-24 00:06:11 +0900

    Initial commit

---
+++ src/com/company/GameFrame.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import javax.swing.*;
+
+public class GameFrame extends JFrame {
+
+    public GameFrame(){
+        this.add(new GamePanel());
+        this.setTitle("Snake");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+    }
+}
